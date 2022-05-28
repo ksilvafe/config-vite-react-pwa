6e2c14c50c2e4098fc026015523510c9cb835ea8
ksilvafe <ferreirakevin325@gmail.com>
2022-05-28 15:26:58 -0300

    config pwa vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,41 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(), 
-    VitePWA({})
-  ]
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Name of your app',
+        short_name: 'Short name of your app',
+        description: 'Description of your app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      },
+      workbox: {
+        sourcemap : true,
+        cleanupOutdatedCaches: false
+      },
+      strategies: 'injectManifest',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]    
 })
